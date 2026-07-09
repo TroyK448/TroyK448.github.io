71cd73b25fe2cff7ebfe9124f79dbf7f67669e8b
TroyK448 <troy.d.kimbrough@gmail.com>
2026-07-09 14:28:50 -0400

    Fix syntax error in marker3 popup binding

--- travelmap.js
+++ travelmap.js
@@ -11,4 +11,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Tokyo!</p>").addTo(map);
 var marker1 = L.circle(place1, {color: "black", fillColor: "#8b008b", radius: 150}).bindPopup("<p>Hello, from Shibuya!</p>").addTo(map); 
 var marker2 = L.circle(place2, {color: "black", fillColor: "#0000ff", radius: 150}).bindPopup("<p>Hello, from Osaka!</p>").addTo(map);
-var marker3 = L.circle(place3, {color: "black", fillColor: "#e34234", radius: 150}).bindPopup("<p>Hello, from Kyoto!,/p>".addTo(map);
+var marker3 = L.circle(place3, {color: "black", fillColor: "#e34234", radius: 150}).bindPopup("<p>Hello, from Kyoto!,/p>").addTo(map);
