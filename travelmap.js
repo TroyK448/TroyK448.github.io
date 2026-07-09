b040576afd4552ff4b4b71d8ee65f70dc4500028
TroyK448 <troy.d.kimbrough@gmail.com>
2026-07-09 14:17:48 -0400

    Update markers and popups in travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -8,7 +8,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker(coordinates).bindPopup("<p>Hello, from Tokyo, Japan!</p>").addTo(map);
-var marker1 = L.circle(place1, {color: "black", fillColor: "#8b008b", radius: 80}).addTo(map); 
-var marker2 = L.circle(place2, {color: "black", fillColor: "#0000ff", radius: 80}).addTo(map);
-var marker3 = L.circle(place3, {color: "black", fillColor: "#e34234", radius: 80}).addTo(map);
+var marker = L.marker(coordinates).bindPopup("<p>Hello, from Tokyo!</p>").addTo(map);
+var marker1 = L.circle(place1, {color: "black", fillColor: "#8b008b", radius: 150}).bindPopup("<p>Hello, from Shibuya!</p>").addTo(map); 
+var marker2 = L.circle(place2, {color: "black", fillColor: "#0000ff", radius: 150}).bindPopup("<p>Hello, from Osaka!</p>").addTo(map);
+var marker3 = L.circle(place3, {color: "black", fillColor: "#e34234", radius: 150}).bindPopup("<p>Hello, from Kyoto!,/p>".addTo(map);
